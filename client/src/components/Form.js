8aa479957fc7931e62ed0e00aa93962ef3c32456
Code Dragi { } <87379592+Dragibus76@users.noreply.github.com>
2022-01-27 18:18:34 +0100

    card component
    card component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Form = () => {
     const [moviesData, setMoviesData] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key={TMDB_API}&query=star&language=fr-FR`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=84483b5fbf72908a4d3869434376a6ba&query=star&language=fr-FR`)
             .then((res) => setMoviesData(res.data.results));
     }, [])
 
@@ -26,9 +27,9 @@ const Form = () => {
                 </div>
             </div>
             <div className='result'>
-                {moviesData.map((movie) => 
-                    <h2>{movie.title}</h2>
-                )}
+                {moviesData.slice(0, 12).map((movie) => (
+                    <Card key={movie.id} movie={movie}/>
+                ))}
             </div>
         </div>
     );
